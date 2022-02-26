180870daca045d4fb00872ba99c7e86984acf6cf
WUMANHO <wumanho@tutanota.com>
2022-02-26 09:48:15 +0800

    first commit

---
+++ sum.ts
@@ -0,0 +1,15 @@
+function twoSum(nums: number[], target: number): number[] {
+    if(!nums || nums.length <1)return[-1,-1]
+    const map = new Map<number,number>()
+    for (let i = 0; i < nums.length; i++) {
+        if(map.has(target - nums[i])){
+            const key = map.get(target - nums[i]) as number
+            return [i,key]
+        }else{
+            map.set(nums[i],i)
+        }
+    }
+    return [-1,-1]
+}
+
+console.log(twoSum([2,7,11,15],9))
